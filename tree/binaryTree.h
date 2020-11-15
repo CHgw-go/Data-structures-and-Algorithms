b95b94170e9a74e3e85f6997b4635183d929dda7
CHgw <73273245+CHgw-go@users.noreply.github.com>
2020-11-15 17:47:26 +0800

    binarytree

---
+++ tree/binaryTree.h
@@ -0,0 +1,186 @@
+#pragma once
+#include<iostream>
+template<class T>
+class binaryTreeNode
+{
+public:
+	T element;
+	binaryTreeNode<T>* left;
+	binaryTreeNode<T>* right;
+	binaryTreeNode()
+	{
+		left = nullptr;
+		right = nullptr;
+	}
+	binaryTreeNode(T e)
+	{
+		left = nullptr;
+		right = nullptr;
+		element = e;
+	}
+	binaryTreeNode(T e, binaryTreeNode<T>* left, binaryTreeNode<T>* right)
+	{
+		this->left = left;
+		this->right = right;
+		element = e;
+	}
+};
+
+template<class T>
+class binaryTree
+{
+	typedef binaryTreeNode<T> node;
+	typedef binaryTree<T> tree;
+
+protected:
+	int treeSize;
+	node* root;
+	static void(*visit)(node*);
+	static void addCount(node*)
+	{
+		++count;
+	}
+	static int count;
+	static void putout(node* theNode)
+	{
+		std::cout << theNode->element << "  ";
+	}
+	static void dispose(node* theNode)
+	{
+		delete theNode;
+	}
+	static int height(node* t);
+public:
+	binaryTree()
+	{
+		root = nullptr;
+		treeSize = 0;
+	}
+	~binaryTree() {erase();}
+
+	void makeTree(const T& theElement, binaryTree<T>& theLeft, binaryTree<T>& theRight);
+	bool empty() const { return treeSize == 0; }
+	int size() const { return treeSize; }
+	int height() const { return height(root); }
+	void erase()
+	{
+		visit = dispose;
+		postOrder(root);
+		root = nullptr;
+		treeSize = 0;
+	}
+	int countNodes(node* theNode)
+	{
+		count = 0;
+		visit = addCount();
+		preOrder(node* theNode);
+	}
+
+	void putout_pre()
+	{
+		visit = &putout;
+		preOrder(root);
+		std::cout << std::endl;
+	}
+	void putout_in()
+	{
+		visit = &putout;
+		inOrder(root);
+		std::cout << std::endl;
+	}
+	void putout_post()
+	{
+		visit = putout;
+		postOrder(root);
+		std::cout << std::endl;
+	}
+
+	binaryTree<T>& removeLeftSubtree();
+	binaryTree<T>& removeRightSubtree();
+
+	void preOrder(node*);
+	void inOrder(node*);
+	void postOrder(node*);
+	//void levelOrder(node*);
+};
+
+template<class T>
+inline void binaryTree<T>::makeTree(const T& theElement, binaryTree<T>& theLeft, binaryTree<T>& theRight)
+{
+	root = new node(theElement, theLeft.root, theRight.root);
+	treeSize = theLeft.size() + theRight.size()+1;
+}
+
+template<class T>
+inline binaryTree<T>& binaryTree<T>::removeLeftSubtree()
+{
+	if (treeSize == 0)
+	{
+		return;
+	}
+	tree leftSubtree;
+	leftSubtree.root = root->left;
+	leftSubtree.treeSize = countNodes(root->left);
+	root->left = nullptr;
+	treeSize = treeSize - leftSubtree.treeSize;
+}
+
+template<class T>
+inline binaryTree<T>& binaryTree<T>::removeRightSubtree()
+{
+	if (treeSize == 0)
+	{
+		return;
+	}
+	tree rightSubtree;
+	rightSubtree.root = root->right;
+	rightSubtree.treeSize = countNodes(root->right);
+	root->right = nullptr;
+	treeSize = treeSize - rightSubtree.treeSize;
+}
+
+template<class T>
+inline void binaryTree<T>::preOrder(node* theRoot)
+{
+	if (theRoot != nullptr)
+	{
+		visit(theRoot);
+		preOrder(theRoot->left);
+		preOrder(theRoot->right);
+	}
+}
+
+template<class T>
+inline void binaryTree<T>::inOrder(node* theRoot)
+{
+	if (theRoot != nullptr)
+	{
+		inOrder(theRoot->left);
+		visit(theRoot);
+		inOrder(theRoot->right);
+	}
+}
+
+template<class T>
+inline void binaryTree<T>::postOrder(node* theRoot)
+{
+	if (theRoot != nullptr)
+	{
+		postOrder(theRoot->left);
+		postOrder(theRoot->right);
+		visit(theRoot);
+	}
+}
+
+template <class T>
+inline int binaryTree<T>::height(node* t)
+{
+	if (t == nullptr)
+		return 0;                  
+	int hl = height(t->left); 
+	int hr = height(t->right);
+	if (hl > hr)
+		return ++hl;
+	else
+		return ++hr;
+}
---
+++ tree/tree.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include"binaryTree.h"
+using namespace std;
+
+template<class T>
+void(*binaryTree<T>::visit)(binaryTreeNode<T>*) = nullptr;
+
+int main()
+{
+    binaryTree<int> a, b, c, d, x;
+    b.makeTree(1, a, a);
+    c.makeTree(2, a, a);
+    d.makeTree(3, b, c);
+    x.makeTree(4, d, a);
+    cout << "Number of nodes = ";
+    cout << x.size() << endl;
+    cout << "height = ";
+    cout << x.height() << endl;
+    cout << "Preorder sequence is ";
+    x.putout_pre();
+    cout << "Inorder sequence is ";
+    x.putout_in();
+    cout << "Postorder sequence is ";
+    x.putout_post();
+    //cout << "Level order sequence is ";
+    //y.levelOrderOutput();
+
+    return 0;
+}
+
